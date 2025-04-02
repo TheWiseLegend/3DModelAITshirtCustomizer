879a1ea4eba5e0f1db5f9b0784800b2ed1bad0d5
TheWiseLegend <B09220027@student.unimy.edu.my>
2025-04-02 15:37:44 +0800

    Initial clean commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import Canvas from './canvas/index.jsx';
+import Cuztomizer from './pages/Customizer.jsx';
+import Home from './pages/Home.jsx';
+function App() {
+    return (
+        <main className='app transition-all ease-in'>
+            <Home />
+            <Cuztomizer />
+            <Canvas />
+        </main>
+    );
+}
+
+export default App;
