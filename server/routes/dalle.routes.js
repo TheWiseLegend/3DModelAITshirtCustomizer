dc2083f9980426b37c7d7cd1d8ae7468b118a1d6
Amr Ibrahim <amribrahim.dev@gmail.com>
2025-11-24 11:35:23 +0800

    test

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -33,6 +33,7 @@ router.route("/").post(async (req, res) => {
         console.error(error);
         res.status(500).json({ message: "Something went wrong" });
     }
+    
 });
 
 export default router;
