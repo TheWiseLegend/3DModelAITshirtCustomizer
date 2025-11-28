2a6d87448b95bb176a6f56d0c5395a00ce63cfbd
Amr Ibrahim <amribrahim.dev@gmail.com>
2025-11-28 15:54:53 +0800

    final complete docker setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,8 +6,9 @@ export default defineConfig({
     plugins: [react()],
     server: {
         port: 5173,
+        host: '0.0.0.0',
         watch: {
-            usePolling: true,
+          usePolling: true,
         },
     },
 });
