32a831663494a5f16aa181770dc32ed5d13cae1f
Amr Ibrahim <amribrahim.dev@gmail.com>
2025-12-01 14:50:28 +0800

    fix port for server

--- server/index.js
+++ server/index.js
@@ -16,6 +16,6 @@ app.get("/", (req, res) => {
     res.status(200).json({ message: "Hello from DALL-E!" });
 });
 
-app.listen(8080, () =>
-    console.log("Server has started on port http://localhost:8080")
-);
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
